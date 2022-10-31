59cc8c08830faf3a129f96fdd5a9f4e483ef2b15
garrisonholt <gholt29@gmail.com>
2022-10-31 17:13:40 -0500

    Server setup and folder structure

--- server/index.js
+++ server/index.js
@@ -1,1 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+const CONNECTION_URL = process.env.MONGO_URL
+
+app.use(bodyParser.json({limit: "30mb", extended: true}))
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
+app.use(cors())
